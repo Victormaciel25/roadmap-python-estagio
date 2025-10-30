7d8cf942184eaf954d8a995acfd6a25a09588c5a
Victor Maciel <victormaciel.dev@gmail.com>
2025-10-30 20:32:12 -0300

    up

---
+++ Fundamentos-da-linguagem/2.py
@@ -0,0 +1,19 @@
+# Python = linguamem de programação
+# Tipo de tipagem = dinâmica/forte
+# str -> string -> texto
+# Strings são textos que estão dentro de aspas
+
+print(1234)
+
+# Aspas simples
+print('Victor Maciel')
+print(1,'Victor Maciel')
+
+# Aspas duplas
+print("Victor Maciel")
+
+# Escape
+print("Victor \"Maciel\"")
+
+# r
+print(r"Victor \"Maciel\"")
